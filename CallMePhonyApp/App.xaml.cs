aa350f981233fc2951b472290d85748fdf0b6ff8
Slupshi <slupshi@gmail.com>
2023-04-24 10:20:16 +0200

    Add project files.

---
+++ CallMePhonyApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CallMePhonyApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ CallMePhonyApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CallMePhonyApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
